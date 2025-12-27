52dc75b136e5ec2a92103564ae5224b8a0965df2
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-12-27 11:26:14 +0100

    Update to XDT99 compatible source

--- make.bat
+++ make.bat
@@ -1,21 +1,20 @@
 @ECHO OFF
 
-xas99.py -R -L bouncy.lst source/bouncy.a99
+xas99.py -q -R -L bouncy.lst source/bouncy.a99
 
 IF %ERRORLEVEL% NEQ 0 GOTO END
 
-xas99.py -R -i source/bouncy.a99 -o IBOUNCY1
+xas99.py -q -R -i source/bouncy.a99 -o bin/BOUNCY1
 
-xdm99.py bouncy.dsk -a IBOUNCY1 -n BOUNCY1
-xdm99.py bouncy.dsk -a IBOUNCY2 -n BOUNCY2
-xdm99.py bouncy.dsk -a IBOUNCY3 -n BOUNCY3
-xdm99.py bouncy.dsk -a IBOUNCY4 -n BOUNCY4
+xdm99.py bouncy.dsk -X dssd -n BOUNCY
+xdm99.py bouncy.dsk -a bin/BOUNCY1 -n BOUNCY1
+xdm99.py bouncy.dsk -a bin/BOUNCY2 -n BOUNCY2
+xdm99.py bouncy.dsk -a bin/BOUNCY3 -n BOUNCY3
+xdm99.py bouncy.dsk -a bin/BOUNCY4 -n BOUNCY4
+xdm99.py bouncy.dsk -t -a disk/LOAD
 
-xdm99.py -T IBOUNCY1 -o BOUNCY1
-xdm99.py -T IBOUNCY2 -o BOUNCY2
-xdm99.py -T IBOUNCY3 -o BOUNCY3
-xdm99.py -T IBOUNCY4 -o BOUNCY4
-
-java -jar tools/ea5tocart.jar IBOUNCY1 "BOUNCY" > make.log
+java -jar tools/ea5tocart.jar bin/BOUNCY1 "BOUNCY" > make.log
+copy bin\bouncy18.bin bouncy8.bin
+jar -cvf bouncy.rpk bouncy8.bin layout.xml > make.log
 
 :END
